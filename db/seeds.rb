d369cb2bbdff9c691e8cc32b45379d085fd453a3
Romain Penaruiz <rom.penaruiz@gmail.com>
2020-04-27 23:09:39 -0400

    last commit, simple airbnb done

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,38 @@ Flat.destroy_all
 
 puts "creating Flats"
 
+# Flat.create!(
+#   name: 'Light & Spacious Garden Flat London',
+#   address: '10 Clifton Gardens London W9 1DT',
+#   photo_url: 'https://source.unsplash.com/random/?loft',
+#   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+#   price_per_night: 75,
+#   number_of_guest: 3
+# )
+
+# Flat.create!(
+#   name: 'Stylish House Close to River Thames',
+#   address: '5 Queensmill Road London SW6 6JP',
+#   photo_url: 'https://source.unsplash.com/random/?appartement',
+#   description: 'Lovely warm comfortable and stylishly furnished house. Private bedroom and bathroom with shared living areas.',
+#   price_per_night: 65,
+#   number_of_guest: 2
+# )
+
+# Flat.create!(
+
+# )
+
 10.times do 
-    flat = Flat.create!(
-        name: Faker::Restaurant.name,
-        address: Faker::Address.street_address,
-        description: Faker::Restaurant.description,
-        price_per_night: rand(20...100),
-        number_of_guest: rand(1..5)
-    )
+  Flat.create!(
+    name: Faker::TvShows::SouthPark.character,
+    address: Faker::Address.street_address,
+    photo_url: 'https://source.unsplash.com/random/?loft',
+    description: Faker::TvShows::SouthPark.quote,
+    price_per_night: rand(15...500),
+    number_of_guest: rand(1...5)
+  )
 end
+
 
 puts "done"
